f539dc3561df5ea9e84072f2ff0eb0f39877ab3b
AntonyCorbett <antony@corbetts.org.uk>
2018-11-26 10:10:11 +0000

    web display

---
+++ OnlyM/Services/WebDisplayManager.cs
@@ -0,0 +1,25 @@
+﻿namespace OnlyM.Services
+{
+    using System;
+    using CefSharp.Wpf;
+
+    internal sealed class WebDisplayManager
+    {
+        private readonly ChromiumWebBrowser _browser;
+
+        public WebDisplayManager(ChromiumWebBrowser browser)
+        {
+            _browser = browser;
+        }
+
+        public void ShowWeb(string mediaItemFilePath)
+        {
+            ////_browser.Address = webAddress.ToString();
+        }
+
+        public void Hide()
+        {
+            _browser.Address = null;
+        }
+    }
+}
