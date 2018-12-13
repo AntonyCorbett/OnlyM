bb251d9976789183d811dbcdf4488a0fc3b865ef
AntonyCorbett <antony@corbetts.org.uk>
2018-12-13 14:48:48 +0000

    work-around for ffmpeg filename bug

--- OnlyM.Core/Utils/FFmpegUtils.cs
+++ OnlyM.Core/Utils/FFmpegUtils.cs
@@ -5,6 +5,8 @@
 
     public static class FFmpegUtils
     {
+        // there is an issue in ffmpeg (https://trac.ffmpeg.org/ticket/819)
+        // - it doesn't accept some file names
         public static Uri FixUnicodeUri(Uri uri)
         {
             return new Uri(FixUnicodeString(uri.ToString()));
@@ -12,7 +14,7 @@
 
         public static string FixUnicodeString(string s)
         {
-            return Encoding.GetEncoding(1252).GetString(Encoding.UTF8.GetBytes(s));
+            return Encoding.GetEncoding(0).GetString(Encoding.UTF8.GetBytes(s));
         }
     }
 }
