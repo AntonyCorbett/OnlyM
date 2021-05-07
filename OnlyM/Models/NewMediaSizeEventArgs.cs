baccf451ee34db1707898735c889e537ce600c56
zokradonh <az@zok.xyz>
2021-05-07 15:03:42 +0200

    Adjust to project convention

--- OnlyM/Models/NewMediaSizeEventArgs.cs
+++ OnlyM/Models/NewMediaSizeEventArgs.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnlyM.Models
+﻿namespace OnlyM.Models
 {
+    using System;
+
     public class NewMediaSizeEventArgs : EventArgs
     {
         public int Width { get; set; }
-        public int Height { get; set; }
 
+        public int Height { get; set; }
     }
 }
