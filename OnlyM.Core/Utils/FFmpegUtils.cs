cf68ee72fdf17528ec99e5becf254ca88093dbe1
AntonyCorbett <antony@corbetts.org.uk>
2021-07-04 12:28:51 +0100

    refs #350

--- OnlyM.Core/Utils/FFmpegUtils.cs
+++ OnlyM.Core/Utils/FFmpegUtils.cs
@@ -5,16 +5,20 @@ namespace OnlyM.Core.Utils
 {
     public static class FFmpegUtils
     {
+        // Jul 2021 - this FFMPEG issue has been fixed so the workaround is no longer needed (hence the commenting out below).
+
         // there is an issue in ffmpeg (https://trac.ffmpeg.org/ticket/819)
         // - it doesn't accept some file names
         public static Uri FixUnicodeUri(Uri uri)
         {
-            return new Uri(FixUnicodeString(uri.ToString()));
+            return uri;
+            //return new Uri(FixUnicodeString(uri.ToString()));
         }
 
         public static string FixUnicodeString(string s)
         {
-            return Encoding.GetEncoding(0).GetString(Encoding.UTF8.GetBytes(s));
+            return s;
+            //return Encoding.GetEncoding(0).GetString(Encoding.UTF8.GetBytes(s));
         }
     }
 }
