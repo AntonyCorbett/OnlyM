294cb2e00cf8d0c8ffb3cc6c61447222ee0741d3
Antony Corbett <antony@corbetts.org.uk>
2024-12-16 10:45:52 +0000

    build params

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,9 +3,9 @@ using System.Resources;
 
 [assembly: AssemblyCompany("SoundBox")]
 [assembly: AssemblyProduct("OnlyM")]
-[assembly: AssemblyCopyright("Copyright © 2018, 2023 Antony Corbett")]
+[assembly: AssemblyCopyright("Copyright © 2018, 2024 Antony Corbett")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 
-[assembly: AssemblyVersion("2.2.0.4")]
+[assembly: AssemblyVersion("2.3.0.1")]
